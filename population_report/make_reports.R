3c30463fe5eabe381d7c747e152d04015d8f7cb6
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2019-06-20 14:30:47 -0500

    Debugging

--- population_report/make_reports.R
+++ population_report/make_reports.R
@@ -2,10 +2,11 @@
 
 library(tidyverse)
 
-# Constants
-config_file <- "population_report/config.yml"
-database_file <- config::get("database", file=config_file)
-asthma_conditions_file <- config::get("asthma_conditions_file", file=config_file)
+# Constants and configuration
+Sys.setenv(R_CONFIG_FILE="population_report/config.yml")
+database_file <- config::get("database")
+asthma_conditions_file <- config::get("asthma_conditions_file")
+query_limit <- config::get("query_limit")
 
 # Load files
 asthma_codes <- read_tsv(asthma_conditions_file)
@@ -14,10 +15,16 @@ asthma_codes <- read_tsv(asthma_conditions_file)
 asthma_concept_names <- asthma_codes %>% select(concept_id=Id, name=Name)
 
 # Connect to database
-con <- DBI::dbConnect(RSQLite::SQLite(), path=database_file, flags=RSQLite::SQLITE_RO)
+con <- DBI::dbConnect(RSQLite::SQLite(), database_file, flags=RSQLite::SQLITE_RO)
 
-condition_occurrence <- tbl(con, 'condition_occurence')
+condition_occurrence <- tbl(con, 'condition_occurrence')
 observation <- tbl(con, 'observation')
+
+# Enforce limits
+if( !is.null(query_limit) && query_limit > 0 ){
+  condition_occurrence <- condition_occurrence %>% head(query_limit)
+  observation <- observation %>% head(query_limit)
+}
 
 # Separate asthma codes into conditions and observations
 asthma_conditions <- asthma_codes %>% filter(Domain == "Condition") %>% pull(Id)
@@ -29,12 +36,12 @@ ehr_asthma_observations <- observation %>% filter(observation %in% asthma_observ
 
 # Merge into one list
 asthma_records <- union_all(
-  ehr_asthma_conditions %>% select(person_id, concept_id=condition_concept_id, date=condition_start_date),
-  ehr_asthma_observations %>% select(person_id, concept_id=observation_concept_id, date=observation_date)
+  ehr_asthma_conditions %>% select(person_id, concept_id=condition_concept_id, the_date=condition_start_date),
+  ehr_asthma_observations %>% select(person_id, concept_id=observation_concept_id, the_date=observation_date)
 )
 
 # Count unique concepts
-asthma_concept_counts <- asthma_records %>% group_by(concept_id) %>% summarize(count=n()) %>% ungroup()
-write_csv(inner_join(asthma_concept_counts, asthma_concept_names, by='concept_id'), file='population_report/concept_counts.csv')
+asthma_concept_counts <- asthma_records %>% group_by(concept_id) %>% summarize(count=n()) %>% ungroup() %>% collect()
+write_csv(inner_join(asthma_concept_counts, asthma_concept_names, by='concept_id'), path='population_report/concept_counts.csv')
 
 # Find the first occurrence for each observation type for each patient
